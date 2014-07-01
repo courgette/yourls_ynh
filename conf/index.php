e273af422e1eb234047fbcd630ed54e18b458a95
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-07-01 07:26:18 +0200

    update manifest

--- conf/index.php
+++ conf/index.php
@@ -3,8 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <title></title>
+  <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
-  <a href="/admin">Admin page</a>
+  <a href="admin">Admin page</a>
 </body>
 </html>
