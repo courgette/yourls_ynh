1dab47a36f6e74c9cfe41cf8668e8556deecd366
anmol <github@datamol.org>
2019-01-04 20:45:48 +0530

    Added admin to the sso

---
+++ conf/add_sso_conf.py
@@ -0,0 +1,11 @@
+import json
+
+with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
+    data = json.load(jsonFile)
+    if "protected_urls" in data:
+        data["protected_urls"].append("__DOMAIN__/__PATH__/admin")
+    else:
+        data["protected_urls"] = ["__DOMAIN__/__PATH__/admin"]
+
+with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
+jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
