4c896e46b9875021fa9a4ea6845631218f52251d
anmol <github@datamol.org>
2019-01-05 00:17:57 +0530

    Fixed error in path for SSO

--- conf/remove_sso_conf.py
+++ conf/remove_sso_conf.py
@@ -2,7 +2,7 @@ import json
 
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
-    data["protected_urls"].remove("__DOMAIN__/__PATH__/admin")
+    data["protected_urls"].remove("__DOMAIN____PATH__/admin")
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
 	jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
