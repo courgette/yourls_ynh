868d52564e0bf55558ddf4c23c9a70c2d6dc2643
ericgaspar <junk.eg@free.fr>
2020-10-09 12:39:26 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,9 @@
 #=================================================
 
 YNH_PHP_VERSION="7.3"
+
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-curl"
+extra_php_dependencies="php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-curl"
 
 #=================================================
 # PERSONAL HELPERS
