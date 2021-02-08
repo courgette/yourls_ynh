ca8991cfdfb30b6735de42187a6154ac1479946d
anmol26s <5068843+anmol26s@users.noreply.github.com>
2021-02-08 07:56:48 +0530

    Admin fix (#12)
    * Update upgrade
    
    * Quick admin fix https://github.com/YunoHost-Apps/yourls_ynh/issues/11
    
    * updated manifest
    
    Co-authored-by: ericgaspar <junk.eg@free.fr>

--- conf/config.php
+++ conf/config.php
@@ -8,20 +8,20 @@
  */
 
 /** MySQL database username */
-define( 'YOURLS_DB_USER', '__DB_NAME__' );
+define('YOURLS_DB_USER', '__DB_NAME__');
 
 /** MySQL database password */
-define( 'YOURLS_DB_PASS', '__DB_PASSWORD__' );
+define('YOURLS_DB_PASS', '__DB_PASSWORD__');
 
 /** The name of the database for YOURLS */
-define( 'YOURLS_DB_NAME', '__DB_NAME__' );
+define('YOURLS_DB_NAME', '__DB_NAME__');
 
 /** MySQL hostname.
  ** If using a non standard port, specify it like 'hostname:port', eg. 'localhost:9999' or '127.0.0.1:666' */
-define( 'YOURLS_DB_HOST', 'localhost' );
+define('YOURLS_DB_HOST', 'localhost');
 
 /** MySQL tables prefix */
-define( 'YOURLS_DB_PREFIX', '__DB_NAME___' );
+define('YOURLS_DB_PREFIX', '__DB_NAME___');
 
 /*
  ** Site options
@@ -29,46 +29,45 @@ define( 'YOURLS_DB_PREFIX', '__DB_NAME___' );
 
 /** YOURLS installation URL -- all lowercase and with no trailing slash.
  ** If you define it to "http://site.com", don't use "http://www.site.com" in your browser (and vice-versa) */
-define( 'YOURLS_SITE', 'https://__DOMAIN____PATH__' );
+define('YOURLS_SITE', 'https://__DOMAIN____PATH__');
 
 /** Timezone GMT offset */
-define( 'YOURLS_HOURS_OFFSET', 0 );
+define('YOURLS_HOURS_OFFSET', 0);
 
 /** YOURLS language or "locale".
  ** Change this setting to "localize" YOURLS (use a translation instead of the default English). A corresponding .mo file
  ** must be installed in the user/language directory.
  ** See http://yourls.org/translations for more information */
-define( 'YOURLS_LANG', '' );
+define('YOURLS_LANG', '');
 
 /** Allow multiple short URLs for a same long URL
  ** Set to true to have only one pair of shortURL/longURL (default YOURLS behavior)
  ** Set to false to allow multiple short URLs pointing to the same long URL (bit.ly behavior) */
-define( 'YOURLS_UNIQUE_URLS', true );
+define('YOURLS_UNIQUE_URLS', true);
 
 /** Private means the Admin area will be protected with login/pass as defined below.
  ** Set to false for public usage (eg on a restricted intranet or for test setups)
  ** Read http://yourls.org/privatepublic for more details if you're unsure */
-define( 'YOURLS_PRIVATE', false );
+define('YOURLS_PRIVATE', false);
 
 /** A random secret hash used to encrypt cookies. You don't have to remember it, make it long and complicated. Hint: copy from http://yourls.org/cookie **/
-define( 'YOURLS_COOKIEKEY', '__RANDOM__' );
+define('YOURLS_COOKIEKEY', '__RANDOM__');
 
 /** Username(s) and password(s) allowed to access the site. Passwords either in plain text or as encrypted hashes
  ** YOURLS will auto encrypt plain text passwords in this file
  ** Read http://yourls.org/userpassword for more information */
-$yourls_user_passwords = array(
-);
+$yourls_user_passwords = array();
 
 /** Debug mode to output some internal information
  ** Default is false for live site. Enable when coding or before submitting a new issue */
-define( 'YOURLS_DEBUG', false );
+define('YOURLS_DEBUG', false);
 
 /*
  ** URL Shortening settings
  */
 
 /** URL shortening method: 36 or 62 */
-define( 'YOURLS_URL_CONVERT', 36 );
+define('YOURLS_URL_CONVERT', 36);
 /*
  * 36: generates all lowercase keywords (ie: 13jkm)
  * 62: generates mixed case keywords (ie: 13jKm or 13JKm)
@@ -76,9 +75,9 @@ define( 'YOURLS_URL_CONVERT', 36 );
  */
 
 /**
-* Reserved keywords (so that generated URLs won't match them)
-* Define here negative, unwanted or potentially misleading keywords.
-*/
+ * Reserved keywords (so that generated URLs won't match them)
+ * Define here negative, unwanted or potentially misleading keywords.
+ */
 $yourls_reserved_URL = array(
   'porn', 'faggot', 'sex', 'nigger', 'fuck', 'cunt', 'dick', 'gay',
 );
@@ -91,3 +90,4 @@ $yourls_reserved_URL = array(
 //define( 'LDAPAUTH_PORT', '389' );
 //define( 'LDAPAUTH_BASE', 'dc=yunohost,dc=org' );
 //define( 'LDAPAUTH_USERNAME_FIELD', 'uid');
+require_once(dirname(__DIR__) . '/includes/functions-auth.php');
